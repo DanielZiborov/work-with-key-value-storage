e081faa62993a8dc6005e338fdd8b1b62190c04c
DanielZiborov <ziborovdanila5@gmail.com>
2024-10-09 12:45:23 +0300

    save and read value by key in SecureStorage

--- lib/example_widget_model.dart
+++ lib/example_widget_model.dart
@@ -1,12 +1,17 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 abstract class _SharedPreferencesKeys {
   static const nameKey = 'name_key';
 }
 
+abstract class _FlutterSecureStorageKeys {
+  static const tokenKey = 'token';
+}
+
 class ExampleWidgetModel {
   final _storage = SharedPreferences.getInstance();
-
+  final _secureStorage = const FlutterSecureStorage();
   Future<void> readName() async {
     final storage = await _storage;
     final name = storage.getString(_SharedPreferencesKeys.nameKey);
@@ -18,7 +23,12 @@ class ExampleWidgetModel {
     await storage.setString(_SharedPreferencesKeys.nameKey, 'Иван');
   }
 
-  Future<void> readToken() async {}
+  Future<void> readToken() async {
+    final token = await _secureStorage.read(key: _FlutterSecureStorageKeys.tokenKey);
+    print(token);
+  }
 
-  Future<void> setToken() async {}
+  Future<void> setToken() async {
+    await _secureStorage.write(key: _FlutterSecureStorageKeys.tokenKey, value: '');
+  }
 }
