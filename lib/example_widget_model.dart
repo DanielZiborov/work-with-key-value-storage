3253adb01594925754b3d3a1b6575c6b7a67a433
DanielZiborov <ziborovdanila5@gmail.com>
2024-10-09 12:24:44 +0300

    _SharedPreferencesKeys

--- lib/example_widget_model.dart
+++ lib/example_widget_model.dart
@@ -1,17 +1,21 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
+abstract class _SharedPreferencesKeys {
+  static const nameKey = 'name_key';
+}
+
 class ExampleWidgetModel {
   final _storage = SharedPreferences.getInstance();
 
   Future<void> readName() async {
     final storage = await _storage;
-    final name = storage.getString('name_key');
+    final name = storage.getString(_SharedPreferencesKeys.nameKey);
     print(name);
   }
 
   Future<void> setName() async {
     final storage = await _storage;
-    await storage.setString('name_key', 'Иван');
+    await storage.setString(_SharedPreferencesKeys.nameKey, 'Иван');
   }
 
   Future<void> readToken() async {}
