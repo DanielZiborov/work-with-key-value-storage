00e8625b69c9df6449ebfd3843b43ceac45d2204
DanielZiborov <ziborovdanila5@gmail.com>
2024-10-09 11:12:42 +0300

    commit1

---
+++ android/app/src/main/kotlin/com/example/work_with_key_value_storage/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.work_with_key_value_storage
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/example_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ExampleWidget extends StatefulWidget {
+  const ExampleWidget({super.key});
+
+  @override
+  State<ExampleWidget> createState() => _ExampleWidgetState();
+}
+
+class _ExampleWidgetState extends State<ExampleWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
